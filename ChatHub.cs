56586ffc666a78ff5a4031f450c31a56722d3c59
sohailbeg <109967438+sohailbeg@users.noreply.github.com>
2024-01-23 01:11:20 +0530

    first commit

---
+++ ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRtesting
+{ 
+    public class ChatHub : Hub
+    {
+       
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+
+        //public async Task SendMessageToCaller(string user, string message)
+        //{
+        //    await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+}
